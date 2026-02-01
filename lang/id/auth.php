aba29059d965d2a11a8daf3f2483b71ee0be1851
buatwebsultra <buatwebsultra@gmail.com>
2026-02-01 20:06:20 +0800

    Update: Terjemahan pesan kesalahan login bahasa Indonesia

---
+++ lang/id/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris-baris Bahasa Autentikasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan selama proses autentikasi untuk berbagai
+    | pesan yang perlu kita tampilkan kepada pengguna. Anda bebas untuk memodifikasi
+    | baris bahasa ini sesuai dengan kebutuhan aplikasi Anda.
+    |
+    */
+
+    'failed' => 'Email atau Password salah.',
+    'password' => 'Password yang diberikan salah.',
+    'throttle' => 'Terlalu banyak upaya login. Silakan coba lagi dalam :seconds detik.',
+
+];
