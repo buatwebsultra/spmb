bf6434f573a50f89b5e0a411f00c4c18f1ff7c75
buatwebsultra <buatwebsultra@gmail.com>
2026-04-11 04:10:32 +0800

    Fix CSS typo in invoice background (container -> contain)

--- resources/views/livewire/pendaftaran-invoice-spp.blade.php
+++ resources/views/livewire/pendaftaran-invoice-spp.blade.php
@@ -1,5 +1,5 @@
 <div class="container container-fluid" style="overflow-x: auto">
-    <div class="d-print-grid border p-3 shadow" style="width: 210mm; background-image: url({{$data['bgspp']}}); background-repeat: no-repeat; background-size: container; background-position: center;">
+    <div class="d-print-grid border p-3 shadow" style="width: 210mm; background-image: url({{$data['bgspp']}}); background-repeat: no-repeat; background-size: contain; background-position: center;">
 
         <div class="row border-bottom d-flex align-items-center pb-2 mb-2">
             <div class="col-2">
